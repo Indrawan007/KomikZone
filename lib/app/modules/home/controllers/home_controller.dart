16767646903ded5a5b001931a2dbfbd5030526b4
Indrawan007 <phantom.el777@gmail.com>
2023-06-09 14:15:48 +0800

    Add Home Page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:komikzone/app/data/model/comics.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  Future<List<Comics>> getAllComics() async {
+    final response = await http.get(
+      Uri.parse('https://wibutools.live/api/komiku'),
+    );
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+    if (response.statusCode == 200) {
+      final List<dynamic> data =
+          (json.decode(response.body) as Map<String, dynamic>)["data"];
+      print(data);
+      return data.map((json) => Comics.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to fetch comics');
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
